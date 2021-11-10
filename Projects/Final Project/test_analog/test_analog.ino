86c350d9fd0c536c6267a6e9eb5a6b7d91fff583
TJ Rosario-Rosa <71331293+trosa051@users.noreply.github.com>
2021-11-10 16:31:30 -0500

    Test Projects
    A few test projects to make sure each input is working as intended. Sources are provided if applicable.

---
+++ Projects/Final Project/test_analog/test_analog.ino
@@ -0,0 +1,21 @@
+was //https://www.instructables.com/Arduino-2-Servos-Thumbstick-joystick/
+
+const int joyH = 3;        // L/R Parallax Thumbstick
+const int joyV = 4;        // U/D Parallax Thumbstick
+
+
+void setup() {
+
+  // Inizialize Serial
+  Serial.begin(9600);
+}
+
+
+void loop(){
+    //Joystick Testing
+    //Serial.println(analogRead(joyH));
+    //Serial.println(analogRead(joyV));
+    //delay(15);
+
+    //240x135 Color TFT Breakout LCD Display Testing
+}
